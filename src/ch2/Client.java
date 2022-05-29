d513b9c5c7b80781ec747d990130649161077172
pursuit <2825841950@qq.com>
2022-05-29 10:38:28 +0800

    优化了项目结构，更加规范

--- src/ch2/Client.java
+++ src/ch2/Client.java
@@ -1,11 +1,15 @@
 package ch2;
 
+import utils.XmlUtils;
+
 public class Client {
     public static void main(String[] args) {
         ImageReaderFactory imageReaderFactory;
         ImageReader imageReader;
-        imageReaderFactory = (ImageReaderFactory) XMLUtils.getBean();
+        // 通过读取config.xml文件来获取类名，使得可以不用修改源程序只需要修改而更改所需要获取的对象
+        imageReaderFactory = (ImageReaderFactory) XmlUtils.getBean("ch2.className");
         imageReader = imageReaderFactory.createImageReader();
+        System.out.println("软开191班 贺志飞 5720191408");
         imageReader.readImage();
     }
 }
--- src/utils/XmlUtils.java
+++ src/utils/XmlUtils.java
@@ -1,4 +1,4 @@
-package ch2;
+package utils;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
@@ -8,18 +8,17 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
 
-public class XMLUtils {
+public class XmlUtils {
     //该方法用于从XML配置文件中提取具体类类名，并返回一个实例对象
-    public static Object getBean() {
+    public static Object getBean(String name) {
         try {
             //创建DOM文档对象
             DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-            Document document;
-            document = documentBuilder.parse(new File("config.xml"));
+            Document document = documentBuilder.parse(new File("config.xml"));
 
             //获取包含类名的文本结点
-            NodeList nodeList = document.getElementsByTagName("ch2.className");
+            NodeList nodeList = document.getElementsByTagName(name);
             Node classNode = nodeList.item(0).getFirstChild();
             String className = classNode.getNodeValue();
 
